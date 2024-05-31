c97cd3b9865264f2aa60526a06d4e36c1058162f
Thyagaraja M <thyagarajsalome@gmail.com>
2024-05-31 10:38:15 +0530

    Portfolio_revision_01

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-};
+document.getElementById('mobile-menu').addEventListener('click', function() {
+  const navList = document.querySelector('.nav-list');
+  navList.classList.toggle('active');
+});
+
